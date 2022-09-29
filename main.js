4e80d1093824170ed72c96bd37906cc0875e631c
Sardor M <70447263+sardorkb@users.noreply.github.com>
2022-09-29 10:21:03 +0400

    Update main.js

--- main.js
+++ main.js
@@ -13,12 +13,12 @@ $(document).ready(function(){
     $("#login_btn").click(function(){
         $("#main").animate({left:"77.5%"},400);
         $("#main").animate({left:"70%"},500);
-        $("#loginform").css("visibility", "hidden");
-        $("#loginform").animate({left:"75%"},400);
+        $("#signupform").css("visibility", "hidden");
+        $("#signupform").animate({left:"75%"},400);
 
-        $("#signupform").animate({left:"83.5%"},400);
-        $("#signupform").animate({left:"70%"},500);
-        $("#signupform").css("visibility", "visible");
+        $("#loginform").animate({left:"83.5%"},400);
+        $("#loginform").animate({left:"70%"},500);
+        $("#loginform").css("visibility", "visible");
     });
     
 });
